c13c2069b17f1959da77598b849821af2049216e
amjadbroung1 <amjad.broung@gmail.com>
2024-07-28 05:02:58 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import x from './images/x.png'
+
+function App() {
+  return (
+    <div className='homeParent'>
+      <div className='homeHeading'>
+        <h1>Great things are coming soon.</h1>
+        <div style={{ textAlign: 'center' }}>
+          First Ape collection on Monad blockchain.{' '}
+        <div>
+
+        <a href='https://twitter.com/monadapes' target='_blank'>
+            <img src={x} className='logo'/>
+          </a>
+        </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
